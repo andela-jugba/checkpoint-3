9732788a063ff990d4e923a6fa74f269ab3ca9d5
Joshua Ugba <justjosh001@gmail.com>
2015-10-16 14:59:36 +0100

    add custom edit text

--- app/src/main/java/com/andela/checkpoint/converter/fragments/ConvertFragment.java
+++ app/src/main/java/com/andela/checkpoint/converter/fragments/ConvertFragment.java
@@ -6,12 +6,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.andela.checkpoint.converter.ConverterCalculator.CurrencySymbols;
 import com.andela.checkpoint.converter.R;
 import com.andela.checkpoint.converter.WheelSelector.Selector;
 import com.andela.checkpoint.converter.calculator.Calculator;
+import com.andela.checkpoint.converter.utilities.CustomTextView;
 
 
 public class ConvertFragment extends Fragment {
@@ -29,6 +31,18 @@ public class ConvertFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_convert, container, false);
         initWheels(v);
+        CustomTextView firstCurrencyEditText = new CustomTextView(v, R.id.editTextFirstCurrency, new CustomTextView.Callback() {
+            @Override
+            public void onTextChange(String s) {
+                Log.i(TAG,s);
+            }
+        });
+        CustomTextView secondCurrencyEditText = new CustomTextView(v, R.id.editTextSecondCurrency, new CustomTextView.Callback() {
+            @Override
+            public void onTextChange(String s) {
+                Log.i(TAG,s);
+            }
+        });
         return v;
     }
 
@@ -36,7 +50,7 @@ public class ConvertFragment extends Fragment {
         Selector firstCurrencyWheel = new Selector(v, CurrencySymbols.CURRENCY_SYMBOLS, R.id.numberpicker, new Selector.CallBack() {
             @Override
             public void onChange(int newVal) {
-
+                Log.i(TAG, newVal + "");
             }
         });
         Selector operatorWheel = new Selector(v, Calculator.operators, R.id.numberpickerOperator, new Selector.CallBack() {
@@ -48,13 +62,13 @@ public class ConvertFragment extends Fragment {
         Selector secondCurrencyWheel = new Selector(v, CurrencySymbols.CURRENCY_SYMBOLS, R.id.numberpickerSecondCurrency, new Selector.CallBack() {
             @Override
             public void onChange(int newVal) {
-
+                Log.i(TAG, newVal + "");
             }
         });
         Selector resultWheel = new Selector(v, CurrencySymbols.CURRENCY_SYMBOLS, R.id.numberpickerResult, new Selector.CallBack() {
             @Override
             public void onChange(int newVal) {
-
+                Log.i(TAG, newVal + "");
             }
         });
 
