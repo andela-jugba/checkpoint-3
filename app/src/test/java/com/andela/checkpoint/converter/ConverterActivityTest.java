b6fb84e24b97abe0c6274251b1da9e4b21600949
Joshua Ugba <justjosh001@gmail.com>
2015-10-16 02:22:29 +0100

    Refactor MainActivity => ConverterActivity

--- app/src/test/java/com/andela/checkpoint/converter/ConverterActivityTest.java
+++ app/src/test/java/com/andela/checkpoint/converter/ConverterActivityTest.java
@@ -2,10 +2,12 @@ package com.andela.checkpoint.converter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertEquals;
+import static org.robolectric.shadows.ShadowView.clickOn;
 
 /**
  * Created by andela on 10/15/15.
@@ -14,8 +16,16 @@ import static org.junit.Assert.assertEquals;
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class,  sdk = 21)
 public class ConverterActivityTest {
+
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+        //Given I am a User
+        //When I am on the Home screen
+        ConverterActivity converterActivity = Robolectric.setupActivity(ConverterActivity.class);
+        //And I tap the Top Ten tab
+        //clickOn();
+
     }
 }
