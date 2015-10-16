3f179c8932207de4a7eed9fd9ac03852e9ac5d6d
Joshua Ugba <justjosh001@gmail.com>
2015-10-16 08:08:40 +0100

    Add Calculator constants class

---
+++ app/src/main/java/com/andela/checkpoint/converter/calculator/Constants.java
@@ -0,0 +1,11 @@
+package com.andela.checkpoint.converter.calculator;
+
+/**
+ * Created by andela on 10/16/15.
+ */
+public class Constants {
+    public static final String ADD = "+";
+    public static final String SUBTRACT = "-";
+    public static final String DIVIDE = "/";
+    public static final String MULTIPLY = "x";
+}
