6939a2235ab2df44869ac3bda6c89e921188c382
Joshua Ugba <justjosh001@gmail.com>
2015-10-16 02:19:57 +0100

    Add Top ten currency value fragment

--- app/src/main/java/com/andela/checkpoint/converter/fragments/TopTenFragment.java
+++ app/src/main/java/com/andela/checkpoint/converter/fragments/TopTenFragment.java
@@ -2,7 +2,8 @@ package com.andela.checkpoint.converter.fragments;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +19,6 @@ public class TopTenFragment extends Fragment {
     public TopTenFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
