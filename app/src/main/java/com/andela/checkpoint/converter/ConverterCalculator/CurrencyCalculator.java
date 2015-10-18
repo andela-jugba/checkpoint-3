402d3c0ab168a1c6976bf093e7328291c9b3e7c0
Joshua Ugba <justjosh001@gmail.com>
2015-10-18 13:40:39 +0100

    refactor convert method

--- app/src/main/java/com/andela/checkpoint/converter/ConverterCalculator/CurrencyCalculator.java
+++ app/src/main/java/com/andela/checkpoint/converter/ConverterCalculator/CurrencyCalculator.java
@@ -7,71 +7,47 @@ import com.andela.checkpoint.converter.calculator.Constants;
  * Created by andela on 10/16/15.
  */
 public class CurrencyCalculator extends Calculator{
+    private final String TAG = "CurrencyCalculator";
+
+
+
+
     private static double baseCurrency;
     private static double targetCurrencyOne;
     private static double targetCurrencyTwo;
 
-    private static boolean baseCurrencySet;
-    private static boolean targetCurrencyOneSet;
-    private static boolean targetCurrencyTwoSet;
+    public CurrencyCalculator(){
+        this.baseCurrency = CurrencySymbols.currencyRates[0];
+        this.targetCurrencyOne = CurrencySymbols.currencyRates[0];
+        this.targetCurrencyTwo = CurrencySymbols.currencyRates[0];
+    }
 
     public static final String[] operators = new String[]{Constants.ADD, Constants.SUBTRACT, Constants.DIVIDE, Constants.MULTIPLY};
 
-    public double convert(double currencyOne, double currencyTwo, String operator, double baseCurrency){
-        if(targetCurrencyOne == targetCurrencyTwo){
-            calculate(operator,currencyTwo,currencyOne);
+    public double convert(double currencyOne, double currencyTwo, String operator){
+
+        if(targetCurrencyOne == targetCurrencyTwo && baseCurrency == targetCurrencyOne && baseCurrency == targetCurrencyTwo){
+            calculate(operator,currencyOne,currencyTwo);
             return getResult();
-        }else{
+        }else if(targetCurrencyOne == targetCurrencyTwo){
+            calculate(operator,currencyOne,currencyTwo);
+             setResult(getResult() * baseCurrency);
+            return getResult();
+        } else {
             calculate(operator, convert(currencyOne,targetCurrencyOne, baseCurrency),convert(currencyTwo,targetCurrencyTwo, baseCurrency));
             return getResult();
         }
     }
     private double convert(double currency, double targetCurrency, double baseCurrency){
-        return currency * targetCurrency * baseCurrency;
-    }
-
-    public static double getBaseCurrency() {
-        return baseCurrency;
+        return currency /targetCurrency * baseCurrency;
     }
 
     public static void setBaseCurrency(double baseCurrency) {
         CurrencyCalculator.baseCurrency = baseCurrency;
     }
 
-    public static boolean isTargetCurrencyTwoSet() {
-        return targetCurrencyTwoSet;
-    }
-
-    public static void setTargetCurrencyTwoSet(boolean targetCurrencyTwoSet) {
-        CurrencyCalculator.targetCurrencyTwoSet = targetCurrencyTwoSet;
-    }
-
-    public static boolean isTargetCurrencyOneSet() {
-        return targetCurrencyOneSet;
-    }
-
-    public static void setTargetCurrencyOneSet(boolean targetCurrencyOneSet) {
-        CurrencyCalculator.targetCurrencyOneSet = targetCurrencyOneSet;
-    }
-
-    public static boolean isBaseCurrencySet() {
-        return baseCurrencySet;
-    }
-
-    public static void setBaseCurrencySet(boolean baseCurrencySet) {
-        CurrencyCalculator.baseCurrencySet = baseCurrencySet;
-    }
-
-    public static double getTargetCurrencyTwo() {
-        return targetCurrencyTwo;
-    }
-
     public static void setTargetCurrencyTwo(double targetCurrencyTwo) {
         CurrencyCalculator.targetCurrencyTwo = targetCurrencyTwo;
-    }
-
-    public static double getTargetCurrencyOne() {
-        return targetCurrencyOne;
     }
 
     public static void setTargetCurrencyOne(double targetCurrencyOne) {
